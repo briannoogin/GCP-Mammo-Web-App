0f8117a2242badedafb65f57367328b49ec56e0d
briannoogin <brian.qh.nguyen@gmail.com>
2019-01-16 04:23:17 -0600

    added web app template

--- mammo-webapp/src/App.js
+++ mammo-webapp/src/App.js
@@ -1,19 +1,74 @@
-import React, { Component } from 'react';
-import './css/App.css';
-import ImageFileDrop from './ImageFileDrop'
-class App extends Component 
-{
+import React from "react";
 
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+const API_KEY = "3585775f387b0d0cba6c5b3dc41b8167";
+
+class App extends React.Component {
+  state = {
+    temperature: 10,
+    city: 'Plano',
+    country: 'United States',
+    humidity: 3,
+    description: 'hot af',
+    error: "Please enter the values."
+  }
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+    if (city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: 10,
+        city: 'Plano',
+        country: 'United States',
+        humidity: 3,
+        description: 'hot af',
+        error: "Please enter the values."
+      });
+    }
+  }
   render() {
     return (
-      <div className="App">
-         <header className="App-header">
-          <h1>Welcome to my mammogram classifier web app!</h1>
-        </header>
-        <ImageFileDrop></ImageFileDrop>
+      <div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-sm-5 col-xs-5 title-container">
+                  <Titles/>
+                </div>
+                <div className="col-sm-7 col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather 
+                    temperature={this.state.temperature} 
+                    humidity={this.state.humidity}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
-}
+};
 
 export default App;
---
+++ mammo-webapp/src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">GCP React Mammography</h1>
+		<h3 className="title-container__subtitle">Leverages Google Cloud ecosystem to classify breast cancer scans!</h3>
+	</div>
+);
+
+export default Titles;

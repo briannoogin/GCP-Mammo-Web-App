7163dc83442d32268257b77c70c0a269524786c2
briannoogin <brian.qh.nguyen@gmail.com>
2019-01-19 00:55:23 -0600

    added a file drop feature

--- mammo-webapp/src/App.js
+++ mammo-webapp/src/App.js
@@ -3,10 +3,14 @@ import React from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
+import ImageFileDrop from "./components/ImageFileDrop"
+import BoxCard from "./components/BoxCard"
 
 const API_KEY = "3585775f387b0d0cba6c5b3dc41b8167";
 
-class App extends React.Component {
+//const {auth} = require('google-auth-library');
+class App extends React.Component 
+{
   state = {
     temperature: 10,
     city: 'Plano',
@@ -41,7 +45,8 @@ class App extends React.Component {
       });
     }
   }
-  render() {
+  render() 
+  {
     return (
       <div>
         <div className="wrapper">
@@ -52,15 +57,16 @@ class App extends React.Component {
                   <Titles/>
                 </div>
                 <div className="col-sm-7 col-xs-7 form-container">
-                  <Form getWeather={this.getWeather} />
+                  {/* <Form getWeather={this.getWeather} />
                   <Weather 
                     temperature={this.state.temperature} 
                     humidity={this.state.humidity}
                     city={this.state.city}
                     country={this.state.country}
                     description={this.state.description}
-                    error={this.state.error}
-                  />
+                    error={this.state.error} 
+                  /> */}
+                  <BoxCard></BoxCard>
                 </div>
               </div>
             </div>
@@ -68,6 +74,23 @@ class App extends React.Component {
         </div>
       </div>
     );
+  }
+
+  async predict(img)
+  {
+    const link = 'https://ml.googleapis.com/v1/projects/MammoWebApp_Model:predict?key={YOUR_API_KEY}';
+    const parameters = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        predictions:img,
+      })
+    };
+    const api_call = await fetch(link,parameters);
+    const data = await api_call.json();
   }
 };
 
--- mammo-webapp/src/components/ImageFileDrop.js
+++ mammo-webapp/src/components/ImageFileDrop.js
@@ -1,17 +1,76 @@
 import React, { Component } from 'react';
 import FileDrop from'react-file-drop'
-import './css/ImageFileDrop.css'
+import '../css/ImageFileDrop.css'
 class ImageFileDrop extends Component 
 {
-    handleDrop = (files, event) => {
-        console.log(files, event);
-      }
-    render() {
-      const styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
+    handleDrop = (files, event) => 
+    {
+        // get file from list of files
+        const file = files['0'];
+        let array_reader = new FileReader();
+        let data_reader = new FileReader();
+        let img = undefined; 
+        let img_array = undefined;
+        // function reads image data to get dimensions 
+        data_reader.onload = function()
+        {
+          img = new Image();
+          //console.log(file)
+          img.src = data_reader.result;
+          // read the file as an array buffer
+          array_reader.readAsArrayBuffer(file);
+        }
+        // function that reads the array data
+        array_reader.onload = function()
+        {
+          // function converts array into 2d array based on the img_width
+          function convertArray(array, img_width)
+          {
+            var img_array = [];
+            // convert 1d array to 2d array with shape given by img_width
+            while(array.length) 
+            {
+              img_array.push(array.splice(0,img_width));
+            }
+            // console.log(JSON.stringify(img_array));
+            // console.log([img_array.length, img_array[0].length ]);
+            return img_array;
+            }
+          // convert Uint8Array to normal Array so splice can be used 
+          const array = Array.from(new Uint8Array(array_reader.result)); 
+          // console.log(array_reader.result);
+          // console.log(array.length);
+          img_array = convertArray(array,img.width);
+         
+          // console.log([img_array.length,img_array[0].length]);
+        };
+        // read as img url
+      data_reader.readAsDataURL(file);
+    }
+   
+    async predict(img)
+    {
+      const link = 'https://ml.googleapis.com/v1/projects/MammoWebApp_Model:predict?key={YOUR_API_KEY}';
+      const parameters = {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          predictions:img,
+        })
+      };
+      const api_call = await fetch(link,parameters);
+      const data = await api_call.json();
+  } 
+    render() 
+    {
+      //const styles = { border: '1px solid black', width: 600, color: 'black', padding: 20 };
       return (
-          <div id="react-file-drop" style = {{styles}}>
+          <div id="react-file-drop">
             <FileDrop onDrop={this.handleDrop}>
-              Drop some files here!
+              Drag & Drop File
             </FileDrop>
           </div>
       );
